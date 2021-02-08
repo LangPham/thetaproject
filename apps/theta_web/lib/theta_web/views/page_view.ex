6203687cb9a92feb8d851db4e4f57b576349a781
Lang Pham <phamxuanlang@gmail.com>
2021-02-08 10:37:19 +0700

    Add toc for article

--- apps/theta_web/lib/theta_web/views/page_view.ex
+++ apps/theta_web/lib/theta_web/views/page_view.ex
@@ -1,84 +1,128 @@
 defmodule ThetaWeb.PageView do
-	use ThetaWeb, :view
+  use ThetaWeb, :view
 
-	def markdown(body) do
-		# ThetaWeb.Debug.debug(body, __MODULE__)
-		body
-		|> Earmark.as_html!()
-		|> Floki.parse_fragment!()
-		|> create_webp()
-		|> update_picture()
-		|> Floki.raw_html()
-		|> raw
+  def md_to_floki(body) do
+    body
+    |> Earmark.as_html!()
+    |> Floki.parse_fragment!()
+    |> create_webp()
+    |> update_picture()
+    |> add_id_header()
+  end
 
-	end
+  def markdown(floki) do
+    # ThetaWeb.Debug.debug(body, __MODULE__)
+    floki
+    |> Floki.raw_html()
+    |> raw
+
+  end
+
+  def toc(floki, url) do
+    floki
+    |> Floki.traverse_and_update(
+         fn
+           {"h2", attrs, children} ->
+             href = "#"<> elem(List.first( attrs), 1)
+             child = {"a", [{"href", url <> href}], children}
+             {"li", [], child}
+           tag -> nil
+         end
+       )
+    |> Floki.raw_html()
+    |> raw
+  end
+
+  defp add_id_header(floki) do
+    floki
+    |> Floki.traverse_and_update(
+         fn
+           {"h2", attrs, children} ->
+             text = Floki.text(children)
+             {"h2", [{"id", Slug.slugify(text)}], children}
+#           {"h3", attrs, children} ->
+#             text = Floki.text(children)
+#             {"h3", [{"id", Slug.slugify(text)}], children}
+           tag -> tag
+         end
+       )
+  end
+
+  defp filter_html(floki, list_filter)do
+    count = length(list_filter)
+    case count do
+      0 ->
+        floki
+      x ->
+        {list_split, list_filter} = Enum.split(list_filter, 1)
+        floki = Floki.filter_out(floki, Enum.at(list_split, 0))
+        filter_html(floki, list_filter)
+    end
+
+  end
+
+  def debug(body) do
+    IO.inspect __MODULE__
+    IO.inspect body
+  end
 
 
+  defp create_webp(floki) do
+    path_storage = Application.get_env(:theta_media, :storage)
+    list_path = Path.split(path_storage)
+    {_, list_new} = List.pop_at(list_path, -1)
+    path = Path.join(list_new)
+    dir_upload = List.last(list_path)
 
+    list =
+      floki
+      |> Floki.find("img")
+      |> Floki.attribute("src")
 
+    for file <- list do
+      files = String.replace(file, ~r/^\/#{dir_upload}/, "/#{dir_upload}/lager")
+      files_ext = Path.extname(files)
+      files_webp = String.replace(files, ~r/#{files_ext}/, ".webp")
 
-	def debug(body) do
-		IO.inspect __MODULE__
-		IO.inspect body
-	end
+      if !File.exists?(Path.join(path, files_webp)) do
+        images =
+          Mogrify.open(Path.join(path, file))
+          |> Mogrify.verbose
+          |> Mogrify.resize("750x750")
+          |> Mogrify.format("webp")
+          |> Mogrify.save(path: Path.join(path, files_webp))
+      end
+    end
+    {floki, dir_upload}
+  end
 
-
-	defp create_webp(floki) do
-		path_storage = Application.get_env(:theta_media, :storage)
-		list_path = Path.split(path_storage)
-		{_, list_new} = List.pop_at(list_path, -1)
-		path = Path.join(list_new)
-		dir_upload = List.last(list_path)
-
-		list =
-			floki
-			|> Floki.find("img")
-			|> Floki.attribute("src")
-
-		for file <- list do
-			files = String.replace(file, ~r/^\/#{dir_upload}/, "/#{dir_upload}/lager")
-			files_ext = Path.extname(files)
-			files_webp = String.replace(files, ~r/#{files_ext}/, ".webp")
-
-			if !File.exists?(Path.join(path, files_webp)) do
-				images =
-					Mogrify.open(Path.join(path, file))
-					|> Mogrify.verbose
-					|> Mogrify.resize("750x750")
-					|> Mogrify.format("webp")
-					|> Mogrify.save(path: Path.join(path, files_webp))
-			end
-		end
-		{floki, dir_upload}
-	end
-
-	defp update_picture({floki, dir_upload})do
-		Floki.traverse_and_update(
-			floki,
-			fn
-				{"img", attrs, children} ->
-					file = elem(List.first(attrs), 1)
-					file_webp =
-						file
-						|> String.replace(~r/^\/#{dir_upload}/, "/#{dir_upload}/lager")
-						|> String.replace(~r/(\.)(\w)+/, ".webp")
-					{
-						"picture",
-						[],
-						[
-							{
-								"source",
-								[
-									{"srcset", file_webp},
-									{"type", "image/webp"}
-								],
-								[]
-							},
-							{"img", attrs, []}
-						]
-					}
-				tag -> tag
-			end
-		)
-	end
+  defp update_picture({floki, dir_upload})do
+    Floki.traverse_and_update(
+      floki,
+      fn
+        {"img", attrs, children} ->
+          file = elem(List.first(attrs), 1)
+          file_webp =
+            file
+            |> String.replace(~r/^\/#{dir_upload}/, "/#{dir_upload}/lager")
+            |> String.replace(~r/(\.)(\w)+/, ".webp")
+          {
+            "picture",
+            [],
+            [
+              {
+                "source",
+                [
+                  {"srcset", file_webp},
+                  {"type", "image/webp"}
+                ],
+                []
+              },
+              {"img", attrs, []}
+            ]
+          }
+        tag -> tag
+      end
+    )
+  end
 end
