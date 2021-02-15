a77e43763e491265de0e1573d636fd3208b23b4a
Lang Pham <phamxuanlang@gmail.com>
2021-02-16 01:06:22 +0700

    Edit cache

--- apps/theta_web/lib/theta_web/endpoint.ex
+++ apps/theta_web/lib/theta_web/endpoint.ex
@@ -1,72 +1,73 @@
 defmodule ThetaWeb.Endpoint do
-	use Phoenix.Endpoint, otp_app: :theta_web
+  use Phoenix.Endpoint, otp_app: :theta_web
 
-	# The session will be stored in the cookie and signed,
-	# this means its contents can be read but not tampered with.
-	# Set :encryption_salt if you would also like to encrypt it.
-	@session_options [
-		store: :cookie,
-		key: "_theta_web_key",
-		encryption_salt: "cookie store encryption salt",
-		signing_salt: "cookie store signing salt",
-		key_length: 64,
-#		same_site: "Strict",
-#		secure: true,
-		log: :debug
-	]
+  # The session will be stored in the cookie and signed,
+  # this means its contents can be read but not tampered with.
+  # Set :encryption_salt if you would also like to encrypt it.
+  @session_options [
+    store: :cookie,
+    key: "_theta_web_key",
+    encryption_salt: "cookie store encryption salt",
+    signing_salt: "cookie store signing salt",
+    key_length: 64,
+    #		same_site: "Strict",
+    #		secure: true,
+    log: :debug
+  ]
 
-	socket "/socket",
-	       ThetaWeb.UserSocket,
-	       websocket: true,
-	       longpoll: false
+  socket "/socket",
+         ThetaWeb.UserSocket,
+         websocket: true,
+         longpoll: false
 
-	socket "/live",
-	       Phoenix.LiveView.Socket,
-	       websocket: [
-		       connect_info: [
-			       session: @session_options
-		       ]
-	       ]
+  socket "/live",
+         Phoenix.LiveView.Socket,
+         websocket: [
+           connect_info: [
+             session: @session_options
+           ]
+         ]
 
-	# Serve at "/" the static files from "priv/static" directory.
-	#
-	# You should set gzip to true if you are running phx.digest
-	# when deploying your static files in production.
+  # Serve at "/" the static files from "priv/static" directory.
+  #
+  # You should set gzip to true if you are running phx.digest
+  # when deploying your static files in production.
 
-	plug Plug.Static,
-	     at: "/",
-	     from: :theta_web,
-	     gzip: false,
-	     only: ~w(css fonts images js favicon.ico robots.txt)
+  plug Plug.Static,
+       at: "/",
+       from: :theta_web,
+       gzip: true,
+       only: ~w(css fonts images js favicon.ico robots.txt),
+       cache_control_for_etags: "public"
 
-	plug Plug.Static,
-	     at: "/uploads",
-	     from: "uploads"
+  plug Plug.Static,
+       at: "/uploads",
+       from: "uploads"
 
 
-	# Code reloading can be explicitly enabled under the
-	# :code_reloader configuration of your endpoint.
-	if code_reloading? do
-		socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
-		plug Phoenix.LiveReloader
-		plug Phoenix.CodeReloader
-		plug Phoenix.Ecto.CheckRepoStatus, otp_app: :theta_web
-	end
+  # Code reloading can be explicitly enabled under the
+  # :code_reloader configuration of your endpoint.
+  if code_reloading? do
+    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
+    plug Phoenix.LiveReloader
+    plug Phoenix.CodeReloader
+    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :theta_web
+  end
 
-	plug Phoenix.LiveDashboard.RequestLogger,
-	     param_key: "request_logger",
-	     cookie_key: "request_logger"
+  plug Phoenix.LiveDashboard.RequestLogger,
+       param_key: "request_logger",
+       cookie_key: "request_logger"
 
-	plug Plug.RequestId
-	plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
+  plug Plug.RequestId
+  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
-	plug Plug.Parsers,
-	     parsers: [:urlencoded, :multipart, :json],
-	     pass: ["*/*"],
-	     json_decoder: Phoenix.json_library()
+  plug Plug.Parsers,
+       parsers: [:urlencoded, :multipart, :json],
+       pass: ["*/*"],
+       json_decoder: Phoenix.json_library()
 
-	plug Plug.MethodOverride
-	plug Plug.Head
-	plug Plug.Session, @session_options
-	plug ThetaWeb.Router
+  plug Plug.MethodOverride
+  plug Plug.Head
+  plug Plug.Session, @session_options
+  plug ThetaWeb.Router
 end
