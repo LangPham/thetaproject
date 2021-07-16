6ef473b8b0f1411506abf8e455f0a1ebd82680b3
Lang Pham <phamxuanlang@gmail.com>
2021-07-16 14:12:16 +0700

    add alpine

--- apps/theta_web/assets/js/front.js
+++ apps/theta_web/assets/js/front.js
@@ -1,82 +1,113 @@
 import "../css/front.sass";
-
+import Alpine from 'alpinejs'
 
 // Bulma navbar
 document.addEventListener('DOMContentLoaded', () => {
+  window.Alpine = Alpine;
 
-		function check_webp_feature(feature, callback) {
-		var kTestImages = {
-			lossy: "UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA",
-			lossless: "UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==",
-			alpha: "UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAARBxAR/Q9ERP8DAABWUDggGAAAABQBAJ0BKgEAAQAAAP4AAA3AAP7mtQAAAA==",
-			animation: "UklGRlIAAABXRUJQVlA4WAoAAAASAAAAAAAAAAAAQU5JTQYAAAD/////AABBTk1GJgAAAAAAAAAAAAAAAAAAAGQAAABWUDhMDQAAAC8AAAAQBxAREYiI/gcA"
-		};
-		var img = new Image();
-		img.onload = function () {
-			var result = (img.width > 0) && (img.height > 0);
-			callback(feature, result);
-		};
-		img.onerror = function () {
-			callback(feature, false);
-		};
-		img.src = "data:image/webp;base64," + kTestImages[feature];
-	}
-	check_webp_feature('lossy', function (feature, isSupported) {
-		let body = document.getElementsByTagName("body");
-		if (isSupported) {
-			body[0].classList.add("webp");
-		} else {
-			body[0].classList.add("no-webp");
-		}
-	});
+  Alpine.start();
+  // Alpine.data('main_menu', () => ({
+  //
+  // 	is_laptop: is_laptop,
+  // 	menu_state: false,
+  // 	dropdown_state: {},
+  //
+  // 	click_menu() {
+  //
+  // 		this.menu_state = !this.menu_state || is_laptop;
+  // 	},
+  // 	click_dropdown(id) {
+  // 		console.log(this.dropdown_state);
+  //
+  // 	},
+  // 	init() {
+  // 		this.menu_state = false || is_laptop;
+  // 	}
+  // }))
 
-	// Get all "navbar-burger" elements
-	const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.hamburger'), 0);
 
-	// Check if there are any navbar burgers
-	if ($navbarBurgers.length > 0) {
+  // window.Alpine = Alpine;
+  // window.Alpine.start();
 
-		// Add a click event on each of them
-		$navbarBurgers.forEach(el => {
-			el.addEventListener('click', () => {
 
-				// Get the target from the "data-target" attribute
-				const target = el.dataset.target;
-				const $target = document.getElementById(target);
+  // ALPINE ===============================================================|^|
 
-				// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-				el.classList.toggle('is-active');
-				$target.classList.toggle('is-active');
+  function check_webp_feature(feature, callback) {
+    var kTestImages = {
+      lossy: "UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA",
+      lossless: "UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==",
+      alpha: "UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAARBxAR/Q9ERP8DAABWUDggGAAAABQBAJ0BKgEAAQAAAP4AAA3AAP7mtQAAAA==",
+      animation: "UklGRlIAAABXRUJQVlA4WAoAAAASAAAAAAAAAAAAQU5JTQYAAAD/////AABBTk1GJgAAAAAAAAAAAAAAAAAAAGQAAABWUDhMDQAAAC8AAAAQBxAREYiI/gcA"
+    };
+    var img = new Image();
+    img.onload = function () {
+      var result = (img.width > 0) && (img.height > 0);
+      callback(feature, result);
+    };
+    img.onerror = function () {
+      callback(feature, false);
+    };
+    img.src = "data:image/webp;base64," + kTestImages[feature];
+  }
 
-			});
-		});
-	}
+  check_webp_feature('lossy', function (feature, isSupported) {
+    let body = document.getElementsByTagName("body");
+    if (isSupported) {
+      body[0].classList.add("webp");
+    } else {
+      body[0].classList.add("no-webp");
+    }
+  });
 
-	//Get the button:
-	let topbutton = document.getElementById("topBtn");
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.hamburger'), 0);
 
-	// When the user scrolls down 20px from the top of the document, show the button
-	window.onscroll = function () {
-		scrollFunction()
-	};
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
 
-	function scrollFunction() {
-		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-			topbutton.style.visibility = "visible";
-			topbutton.style.opacity = "1";
-		} else {
-			topbutton.style.visibility = "hidden";
-			topbutton.style.opacity = "0";
-		}
-	}
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
 
-	topbutton.addEventListener("click", topFunction);
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
 
-	// When the user clicks on the button, scroll to the top of the document
-	function topFunction() {
-		document.body.scrollTop = 0; // For Safari
-		document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-	}
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+
+  // //Get the button:
+  // let topbutton = document.getElementById("topBtn");
+  //
+  // // When the user scrolls down 20px from the top of the document, show the button
+  // window.onscroll = function () {
+  //   scrollFunction()
+  // };
+  //
+  // function scrollFunction() {
+  //   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  //     topbutton.style.visibility = "visible";
+  //     topbutton.style.opacity = "1";
+  //   } else {
+  //     topbutton.style.visibility = "hidden";
+  //     topbutton.style.opacity = "0";
+  //   }
+  // }
+
+  //
+  // topbutton.addEventListener("click", topFunction);
+  //
+  // // When the user clicks on the button, scroll to the top of the document
+  // function topFunction() {
+  //   document.body.scrollTop = 0; // For Safari
+  //   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  // }
 
 });
 
--- apps/theta_web/assets/rollup.config.js
+++ apps/theta_web/assets/rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import {babel} from '@rollup/plugin-babel';
-import { terser } from "rollup-plugin-terser";
+//import { terser } from "rollup-plugin-terser";
 import precss from 'precss';
 import postcss from 'rollup-plugin-postcss';
 import copy from 'rollup-plugin-copy';
@@ -16,7 +16,7 @@ export default [
             format: 'iife',
         },
         plugins: [
-            // babel({babelHelpers: 'bundled'}),
+             // babel({babelHelpers: 'bundled'}),
             postcss({
                 // extract: true,
                 extract: 'css/front.css',
@@ -30,61 +30,66 @@ export default [
                     { src: 'static', dest: '../priv' }
                 ]
             }),
-            terser()
-        ],
-    },
-    {
-        input: 'js/app.js',
-        output: {
-            dir: '../priv/static',
-            name: 'bundle_name',
-            entryFileNames: "js/[name].js",
-            format: 'iife',
-        },
-        plugins: [
             resolve({
                 browser: true
             }),
-            commonjs(),
-            babel({babelHelpers: 'bundled'}),
-            postcss({
-                // extract: true,
-                extract: 'css/app.css',
-                minimize: true,
-                plugins: [
-                    precss(),
-                ]
-            }),
-            terser({
-                format: {
-                    comments: /^1/,
-                },
-            })
+             commonjs(),
+
+            // terser()
         ],
     },
-    {
-        input: 'js/editor.js',
-        output: {
-            dir: '../priv/static',
-            name: 'editor',
-            entryFileNames: "js/[name].js",
-            format: 'iife',
-        },
-        plugins: [
-            resolve({
-                browser: true
-            }),
-            commonjs(),
-            babel({babelHelpers: 'bundled'}),
-            postcss({
-                // extract: true,
-                extract: 'css/editor.css',
-                minimize: true,
-                plugins: [
-                    precss(),
-                ]
-            }),
-            terser()
-        ],
-    }
+    // {
+    //     input: 'js/app.js',
+    //     output: {
+    //         dir: '../priv/static',
+    //         name: 'bundle_name',
+    //         entryFileNames: "js/[name].js",
+    //         format: 'iife',
+    //     },
+    //     plugins: [
+    //         resolve({
+    //             browser: true
+    //         }),
+    //         commonjs(),
+    //         babel({babelHelpers: 'bundled'}),
+    //         postcss({
+    //             // extract: true,
+    //             extract: 'css/app.css',
+    //             minimize: true,
+    //             plugins: [
+    //                 precss(),
+    //             ]
+    //         }),
+    //         terser({
+    //             format: {
+    //                 comments: /^1/,
+    //             },
+    //         })
+    //     ],
+    // },
+    // {
+    //     input: 'js/editor.js',
+    //     output: {
+    //         dir: '../priv/static',
+    //         name: 'editor',
+    //         entryFileNames: "js/[name].js",
+    //         format: 'iife',
+    //     },
+    //     plugins: [
+    //         resolve({
+    //             browser: true
+    //         }),
+    //         commonjs(),
+    //         babel({babelHelpers: 'bundled'}),
+    //         postcss({
+    //             // extract: true,
+    //             extract: 'css/editor.css',
+    //             minimize: true,
+    //             plugins: [
+    //                 precss(),
+    //             ]
+    //         }),
+    //         terser()
+    //     ],
+    // }
 ];
