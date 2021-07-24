b3049946eaab0af972f7c92a09c95df970b72c8c
LangPham <phamxuanlang@gmail.com>
2021-07-24 14:03:52 +0700

    Ok for layout

--- apps/theta_web/assets/js/front.js
+++ apps/theta_web/assets/js/front.js
@@ -4,139 +4,68 @@ import Alpine from 'alpinejs'
 
 // Bulma navbar
 document.addEventListener('DOMContentLoaded', () => {
-  let is_laptop = false;
-  if (window.innerWidth > 1024) {
-    is_laptop = true;
-  }
-
-  Alpine.data('main_menu', () => ({
-
-    is_laptop: is_laptop,
-    menu_state: false,
-    dropdown_state: {},
-    // click_away() {
-    //   this.dropdown_state[id] = !this.dropdown_state[id];
-    // },
-
-    click_menu() {
-      // console.log(this.menu_open)
-      this.menu_state = !this.menu_state || is_laptop;
-    },
-    click_dropdown(id) {
-      console.log(this.dropdown_state);
-      // this.dropdown_state[id] = !this.dropdown_state[id];
-    },
-    init() {
-      this.menu_state = false || is_laptop;
-      // this.dropdown_state = [];
+    let is_laptop = false;
+    if (window.innerWidth > 1024) {
+        is_laptop = true;
     }
-  }))
 
-  window.Alpine = Alpine;
+    Alpine.data('main_menu', () => ({
+        is_laptop: is_laptop,
+        menu_state: false,
+        dropdown_state: {},
+        // click_away() {
+        //   this.dropdown_state[id] = !this.dropdown_state[id];
+        // },
 
-  Alpine.start();
-  // Alpine.data('main_menu', () => ({
-  //
-  // 	is_laptop: is_laptop,
-  // 	menu_state: false,
-  // 	dropdown_state: {},
-  //
-  // 	click_menu() {
-  //
-  // 		this.menu_state = !this.menu_state || is_laptop;
-  // 	},
-  // 	click_dropdown(id) {
-  // 		console.log(this.dropdown_state);
-  //
-  // 	},
-  // 	init() {
-  // 		this.menu_state = false || is_laptop;
-  // 	}
-  // }))
+        click_menu() {
 
+            this.menu_state = !this.menu_state || is_laptop;
+        },
 
-  // window.Alpine = Alpine;
-  // window.Alpine.start();
+        init() {
+            this.menu_state = false || is_laptop;
 
+        }
+    }))
 
-  // ALPINE ===============================================================|^|
+    window.Alpine = Alpine;
 
-  function check_webp_feature(feature, callback) {
-    var kTestImages = {
-      lossy: "UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA",
-      lossless: "UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==",
-      alpha: "UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAARBxAR/Q9ERP8DAABWUDggGAAAABQBAJ0BKgEAAQAAAP4AAA3AAP7mtQAAAA==",
-      animation: "UklGRlIAAABXRUJQVlA4WAoAAAASAAAAAAAAAAAAQU5JTQYAAAD/////AABBTk1GJgAAAAAAAAAAAAAAAAAAAGQAAABWUDhMDQAAAC8AAAAQBxAREYiI/gcA"
-    };
-    var img = new Image();
-    img.onload = function () {
-      var result = (img.width > 0) && (img.height > 0);
-      callback(feature, result);
-    };
-    img.onerror = function () {
-      callback(feature, false);
-    };
-    img.src = "data:image/webp;base64," + kTestImages[feature];
-  }
+    Alpine.start();
 
-  check_webp_feature('lossy', function (feature, isSupported) {
-    let body = document.getElementsByTagName("body");
-    if (isSupported) {
-      body[0].classList.add("webp");
-    } else {
-      body[0].classList.add("no-webp");
+    // ALPINE ===============================================================|^|
+
+    function check_webp_feature(feature, callback) {
+        var kTestImages = {
+            lossy: "UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA",
+            lossless: "UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==",
+            alpha: "UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAARBxAR/Q9ERP8DAABWUDggGAAAABQBAJ0BKgEAAQAAAP4AAA3AAP7mtQAAAA==",
+            animation: "UklGRlIAAABXRUJQVlA4WAoAAAASAAAAAAAAAAAAQU5JTQYAAAD/////AABBTk1GJgAAAAAAAAAAAAAAAAAAAGQAAABWUDhMDQAAAC8AAAAQBxAREYiI/gcA"
+        };
+        var img = new Image();
+        img.onload = function () {
+            var result = (img.width > 0) && (img.height > 0);
+            callback(feature, result);
+        };
+        img.onerror = function () {
+            callback(feature, false);
+        };
+        img.src = "data:image/webp;base64," + kTestImages[feature];
     }
-  });
 
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.hamburger'), 0);
-
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-
-    // Add a click event on each of them
-    $navbarBurgers.forEach(el => {
-      el.addEventListener('click', () => {
-
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
-      });
+    check_webp_feature('lossy', function (feature, isSupported) {
+        let main = document.getElementsByTagName("main");
+        if (isSupported) {
+            main[0].style.backgroundImage = "url(/images/bg.webp)";
+        } else {
+            main[0].style.backgroundImage = "url(/images/bg.png)";
+        }
     });
-  }
 
-
-  // //Get the button:
-  // let topbutton = document.getElementById("topBtn");
-  //
-  // // When the user scrolls down 20px from the top of the document, show the button
-  // window.onscroll = function () {
-  //   scrollFunction()
-  // };
-  //
-  // function scrollFunction() {
-  //   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-  //     topbutton.style.visibility = "visible";
-  //     topbutton.style.opacity = "1";
-  //   } else {
-  //     topbutton.style.visibility = "hidden";
-  //     topbutton.style.opacity = "0";
-  //   }
-  // }
-
-  //
-  // topbutton.addEventListener("click", topFunction);
-  //
-  // // When the user clicks on the button, scroll to the top of the document
-  // function topFunction() {
-  //   document.body.scrollTop = 0; // For Safari
-  //   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  // }
+    // Get all "navbar-burger" elements
+    let hamburger = document.getElementById('hamburger')
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('is-active');
+    });
 
 });
 
